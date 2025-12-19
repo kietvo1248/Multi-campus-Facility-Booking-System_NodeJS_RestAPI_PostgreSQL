4cdb50b098723e188c6cc5115c7c69b24eaf21ca
kietvo1248 <kietvo.011203@gmail.com>
2025-12-19 21:39:31 +0700

    view report list &detail

--- src/interfaces/routes/ReportRoutes.js
+++ src/interfaces/routes/ReportRoutes.js
@@ -5,6 +5,19 @@ const createReportRouter = (controller) => {
   const router = express.Router()
   router.post('/booking', authenticate, authorize(['SECURITY_GUARD','ADMIN','FACILITY_ADMIN']), (req,res)=>controller.reportBooking(req,res))
   router.post('/facility/:facilityId', authenticate, authorize(['SECURITY_GUARD','ADMIN','FACILITY_ADMIN']), (req,res)=>controller.reportFacility(req,res))
+
+  router.get('/', 
+    authenticate, 
+    authorize(['FACILITY_ADMIN', 'CAMPUS_ADMIN', 'ADMIN']), 
+    (req, res) => controller.list(req, res)
+  );
+
+  // 2. Xem chi tiết
+  router.get('/:id', 
+    authenticate, 
+    authorize(['FACILITY_ADMIN', 'CAMPUS_ADMIN', 'ADMIN']), 
+    (req, res) => controller.getDetail(req, res)
+  );
   return router
 }
 
